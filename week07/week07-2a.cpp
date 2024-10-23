f946202680c5469588b36357f33f70dfdd28221f
Lee-Z-Han <cindy20050116@gmail.com>
2024-10-23 11:47:17 +0800

    week07

--- week07/week07-2a.cpp
+++ week07/week07-2a.cpp
@@ -1,5 +1,5 @@
-// week07-1.cpp 還沒有完成的的 LeetCode 學習計畫，只做讀2D陣列、畫2D陣列
-// 只做讀2D陣列、模擬、畫2D陣列
+// week07-2.cpp LeetCode 學習計畫 Simulation第3題
+// 讀2D陣列、模擬(1，2兩人)、畫2D陣列
 // LeetCode 1275. Find Winner on a Tic Tac Toe Game
 class Solution {
 public:
@@ -14,11 +14,13 @@ public:
     }
     string tictactoe(vector<vector<int>>& moves) {
         int board[3][3] = {}; // Step03：C的陣列，裡面都放0代表沒有人放東西
+        int now = 1; // 1,2,1,2 (把 now = 3 - now 就會跳動了)
         myDrawBoard(board);
         for(auto move : moves){ // Step01：C++進階迴圈
             int i = move[0], j = move[1]; // Step02：取出陣列裡的值
             board[i][j] = 1;
             myDrawBoard(board);
+			now = 3 - now;
         }
         return "B"; // "A" or "B" or "Draw"平手
     }
